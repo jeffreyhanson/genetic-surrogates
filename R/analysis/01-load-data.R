10849c53ee91165877ee161289d341e4cd05ed92
paleo13 <jeffrey.hanson@uqconnnect.edu.au>
2016-01-24 14:23:26 +1000

    + account for species with individuals with no data + fix captions in supplementary info

--- R/analysis/01-load-data.R
+++ R/analysis/01-load-data.R
@@ -2,6 +2,9 @@
 session::restore.session('results/.cache/00-initialization.rda')
 
 ## compile spatial grid data
+# all.spp <- seq_len(general.params.LST[[MODE]]$n.spp)
+all.spp <- c(1:3, 23)
+
 # load grid cell centroids
 grid.DF <- fread(
 	'data/Data_Meirmans_et_al_IntrabioDiv/ReadMe.txt',
@@ -19,7 +22,7 @@ spp.aflp.paths <- dir(
 	'data/Data_Meirmans_et_al_IntrabioDiv',
 	'^.*AFLP\\.dat$',
 	full.names=TRUE
-)[seq_len(general.params.LST[[MODE]]$n.spp)]
+)[all.spp]
 
 spp.BayeScanData.LST <- llply(
 	spp.aflp.paths,
@@ -37,7 +40,7 @@ spp.loc.paths <- dir(
 	'data/Data_Meirmans_et_al_IntrabioDiv',
 	'^.*locations\\.txt$',
 	full.names=TRUE
-)[seq_len(general.params.LST[[MODE]]$n.spp)]
+)[all.spp]
 spp.samples.DF <- ldply(
 	seq_along(spp.loc.paths),
 	.fun=function(i) {
@@ -56,13 +59,31 @@ spp.samples.DF <- ldply(
 		by='cell'
 )
 
-# append species data to grid data.frame (wide-format)
+## remove indidivduals that are all NAs
+for (i in seq_along(spp.StructureData.LST)) {
+	# find individuals that are all NAs
+	curr.invalid <- which(rowSums(is.na(spp.StructureData.LST[[i]]@matrix))==ncol(spp.StructureData.LST[[i]]@matrix))
+	# if any invalid then remove from objects
+	if (length(curr.invalid)>0) {
+		# get valid individuals
+		curr.valid <- which(rowSums(is.na(spp.StructureData.LST[[i]]@matrix))!=ncol(spp.StructureData.LST[[i]]@matrix))
+		# subset from StructureData
+		spp.StructureData.LST[[i]] <- structurer:::sample.subset.StructureData(spp.StructureData.LST[[i]], curr.valid)
+		# subset from BayescanData
+		spp.BayeScanData.LST[[i]] <- bayescanr:::sample.subset.BayeScanData(spp.BayeScanData.LST[[i]], curr.valid)
+		# remove from spp.samples.DF
+		spp.samples.DF <- spp.samples.DF[-which(spp.samples.DF$species==unique(spp.samples.DF$species)[i])[curr.invalid],]
+	}
+}
+
+## append species data to grid data.frame (wide-format)
 for (i in unique(spp.samples.DF$species))
 	grid.DF[[i]] <- replace(
 		rep(0, nrow(grid.DF)),
 		which(grid.DF$cell %in% filter(spp.samples.DF, species==i)$cell),
 		1
 	)
+
 
 ## save .rda
 save.session('results/.cache/01-load-data.rda')
